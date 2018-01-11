9684665027de64aae729a0144fede6c715e5ca8e
Zach Bauer <zbauer91@gmail.com>
2018-01-11 08:41:03 -0500

    added useMongoClient

--- db/connection.js
+++ db/connection.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose')
 mongoose.Promise = Promise
 
 if (process.env.NODE_ENV == "production") {
-  mongoose.connect(process.env.MLAB_URL)
+  mongoose.connect(process.env.MLAB_URL, {useMongoClient: true})
 } else {
-  mongoose.connect("mongodb://localhost/posts");
+  mongoose.connect("mongodb://localhost/posts", {useMongoClient: true});
 }
 
 module.exports = mongoose
